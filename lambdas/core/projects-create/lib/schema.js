7940832a0b453c3680ec3d08a487e1a161206112
Jose Mejia Martinez <josemejiamartinez@MacBook-Air-de-Jose.local>
2024-11-14 08:47:22 -0500

    .

--- lambdas/core/projects-create/lib/schema.js
+++ lambdas/core/projects-create/lib/schema.js
@@ -1,6 +1,6 @@
 const josmejia2401js = require('josmejia2401-js');
 exports.schema = josmejia2401js.joi.object({
     name: josmejia2401js.joi.string().max(100).required(),
-    description: josmejia2401js.joi.string().max(250).required()
+    description: josmejia2401js.joi.string().max(250).optional()
 });
 
