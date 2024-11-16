417a1c41b49134a069cef1d7556b673fe7522814
Jose Mejia Martinez <josemejiamartinez@MacBook-Air-de-Jose.local>
2024-11-16 11:52:06 -0500

    .

--- lambdas/core/functionalities-update-by-id/lib/schema.js
+++ lambdas/core/functionalities-update-by-id/lib/schema.js
@@ -3,4 +3,5 @@ exports.schema = josmejia2401js.joi.object({
     name: josmejia2401js.joi.string().max(100).required(),
     description: josmejia2401js.joi.string().allow("").max(1000).optional(),
     status: josmejia2401js.joi.number().required(),
+    projectId: josmejia2401js.joi.string().max(30).required(),
 });
